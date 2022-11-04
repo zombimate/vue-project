f8921e640244c81cea2fcfc4a73ab300c58447e4
zombimate <113879231+zombimate@users.noreply.github.com>
2022-11-04 16:36:02 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav>
+    <router-link to = "/">Home</router-link> |
+    <router-link to = "/Count">Count</router-link> |
+    <router-link to = "/Computed">Computed</router-link> |
+    <router-link to = "/Conditional">Conditional</router-link> |
+    <router-link to = "/PostCode">PostCode</router-link> |
+  </nav>
+  <router-view />
+</template>
---
+++ pages/Conditional.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="awesome = !awesome">전환</button>
+
+  <h1 v-if="awesome">Vue는 정말 멋지죠!</h1>
+  <h1 v-else>아닌가요? 😢</h1>
+</template>
+<script>
+export default {
+  data: {
+    awesome: true,
+  },
+  else: {
+    awesome: false,
+  }
+}
+</script>
